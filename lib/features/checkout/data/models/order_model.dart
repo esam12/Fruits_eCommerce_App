d0702e991ff960f98caaeb3cd06341188d98b610
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-06-01 19:58:39 +0300

    add orderId and date to OrderModel - Added UUID generation for orderId - Added date field with current timestamp - Updated toJson() to include new fields - Updated constructor and fromEntity factory to handle orderId

--- lib/features/checkout/data/models/order_model.dart
+++ lib/features/checkout/data/models/order_model.dart
@@ -1,6 +1,7 @@
 import 'package:fruits/features/checkout/data/models/order_product_model.dart';
 import 'package:fruits/features/checkout/data/models/shipping_address_model.dart';
 import 'package:fruits/features/checkout/domain/entities/order_entity.dart';
+import 'package:uuid/uuid.dart';
 
 class OrderModel {
   final num totalPrice;
@@ -8,33 +9,36 @@ class OrderModel {
   final ShippingAddressModel shippingAddressModel;
   final List<OrderProductModel> orderProducts;
   final String paymentMethod;
+  final String orderId;
 
-  OrderModel({
-    required this.totalPrice,
-    required this.uId,
-    required this.shippingAddressModel,
-    required this.orderProducts,
-    required this.paymentMethod,
-  });
+  OrderModel(
+      {required this.totalPrice,
+      required this.uId,
+      required this.shippingAddressModel,
+      required this.orderProducts,
+      required this.paymentMethod,
+      required this.orderId});
 
   factory OrderModel.fromEntity(OrderEntity orderEntity) {
     return OrderModel(
-      totalPrice: orderEntity.cartEntity.calculateTotalPrice(),
-      uId: orderEntity.uID,
-      shippingAddressModel:
-          ShippingAddressModel.fromEntity(orderEntity.shippingAddress),
-      orderProducts: orderEntity.cartEntity.cartItems
-          .map((e) => OrderProductModel.fromEntity(cartItemEntity: e))
-          .toList(),
-      paymentMethod: orderEntity.payWithCash! ? 'Cash' : 'PayPal',
-    );
+        totalPrice: orderEntity.cartEntity.calculateTotalPrice(),
+        uId: orderEntity.uID,
+        shippingAddressModel:
+            ShippingAddressModel.fromEntity(orderEntity.shippingAddress),
+        orderProducts: orderEntity.cartEntity.cartItems
+            .map((e) => OrderProductModel.fromEntity(cartItemEntity: e))
+            .toList(),
+        paymentMethod: orderEntity.payWithCash! ? 'Cash' : 'PayPal',
+        orderId: const Uuid().v4());
   }
 
   toJson() {
     return {
       'totalPrice': totalPrice,
       'uId': uId,
+      'orderId': orderId,
       'status': 'Pending',
+      'date': DateTime.now().toString(),
       'shippingAddressModel': shippingAddressModel.toJson(),
       'orderProducts': orderProducts.map((e) => e.toJson()).toList(),
       'paymentMethod': paymentMethod,
