0005b108b459c7c34b8c0d3571d297978124ecac
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-07 22:22:03 +0300

    Add ProfileView to MainViewBody

--- lib/features/home/presentation/views/widgets/main_view_body.dart
+++ lib/features/home/presentation/views/widgets/main_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits/features/home/presentation/views/cart_view.dart';
 import 'package:fruits/features/home/presentation/views/products_view.dart';
+import 'package:fruits/features/profile/presentation/views/profile_view.dart';
 import 'package:fruits/features/home/presentation/views/widgets/home_view.dart';
 
 class MainViewBody extends StatelessWidget {
@@ -19,6 +20,7 @@ class MainViewBody extends StatelessWidget {
         HomeView(),
         ProductsView(),
         CartView(),
+        ProfileView(),
       ],
     );
   }
