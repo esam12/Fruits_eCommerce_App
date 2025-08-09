8b7b164e1f313f8126b77a867511821bd2c034a4
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-09 23:47:41 +0300

    update OrderEntity import path to orders feature

--- lib/core/repos/orders_repo/orders_repo.dart
+++ lib/core/repos/orders_repo/orders_repo.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruits/core/errors/failures.dart';
-import 'package:fruits/features/checkout/domain/entities/order_entity.dart';
+import 'package:fruits/features/orders/domain/entities/order_entity.dart';
 
 abstract class OrdersRepo {
   // Add Order To Firebase
