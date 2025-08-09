efdeb1fb28c8e0b1e4681ff991e0029f9ffe83d0
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-09 23:49:29 +0300

    update OrderEntity import path to orders feature

--- lib/features/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart
+++ lib/features/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart
@@ -1,4 +1,4 @@
-import 'package:fruits/features/checkout/domain/entities/order_entity.dart';
+import 'package:fruits/features/orders/domain/entities/order_entity.dart';
 import 'amount.dart';
 import 'item_list.dart';
 
