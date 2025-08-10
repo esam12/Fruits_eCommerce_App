21a61f6f8e061d0b16812ea36b8c08e60d4ef936
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-10 22:59:19 +0300

    Add getAllOrders and deleteOrder methods to OrdersRepo interface

--- lib/core/repos/orders_repo/orders_repo.dart
+++ lib/core/repos/orders_repo/orders_repo.dart
@@ -1,8 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruits/core/errors/failures.dart';
+import 'package:fruits/features/orders/data/models/order_model.dart';
 import 'package:fruits/features/orders/domain/entities/order_entity.dart';
 
 abstract class OrdersRepo {
   // Add Order To Firebase
   Future<Either<Failure, void>> addOrder({required OrderEntity orderEntity});
+
+  // Get All Orders From Firebase
+  Future<Either<Failure, List<OrderModel>>> getAllOrders();
+
+  Future<Either<Failure, Unit>> deleteOrder(int orderId);
 }
