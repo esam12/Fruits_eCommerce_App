a722a3871b4f13b9e494a1091b00aa5347ad42bd
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-10 22:59:55 +0300

    feat: Implement getAllOrders method in OrdersRepoImpl refactor: Fix import path for OrderModel chore: Add TODO for deleteOrder implementation

--- lib/core/repos/orders_repo/orders_repo_impl.dart
+++ lib/core/repos/orders_repo/orders_repo_impl.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 import 'package:fruits/core/errors/failures.dart';
 import 'package:fruits/core/repos/orders_repo/orders_repo.dart';
 import 'package:fruits/core/services/data_service.dart';
 import 'package:fruits/core/utils/backend_endpoint.dart';
-import 'package:fruits/features/checkout/data/models/order_model.dart';
+import 'package:fruits/features/orders/data/models/order_model.dart';
 import 'package:fruits/features/orders/domain/entities/order_entity.dart';
 
 class OrdersRepoImpl implements OrdersRepo {
@@ -16,13 +18,40 @@ class OrdersRepoImpl implements OrdersRepo {
   Future<Either<Failure, void>> addOrder(
       {required OrderEntity orderEntity}) async {
     try {
-     var orderModel = OrderModel.fromEntity(orderEntity);
+      var orderModel = OrderModel.fromEntity(orderEntity);
       await fireStoreService.addData(
           path: BackendEndpoint.addOrders,
           documentId: orderModel.orderId,
           data: orderModel.toJson());
 
       return right(null);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deleteOrder(int orderId) {
+    // TODO: implement deleteOrder
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<OrderModel>>> getAllOrders() async {
+    try {
+      var rawData =
+          await fireStoreService.getData(path: BackendEndpoint.getOrders);
+
+      final List<Map<String, dynamic>> data =
+          List<Map<String, dynamic>>.from(rawData);
+
+    
+      final orders = data
+          .map<OrderModel>((e) => OrderModel.fromJson(e))
+          .toList();
+
+      log(orders.toString());
+      return right(orders);
     } catch (e) {
       return left(ServerFailure(e.toString()));
     }
