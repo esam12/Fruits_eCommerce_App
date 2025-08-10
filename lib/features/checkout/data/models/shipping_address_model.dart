896283150867f5901b5a15c39125dae320c4bff0
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-10 23:00:57 +0300

    Add fromJson and toEntity methods to ShippingAddressModel

--- lib/features/checkout/data/models/shipping_address_model.dart
+++ lib/features/checkout/data/models/shipping_address_model.dart
@@ -41,6 +41,29 @@ class ShippingAddressModel {
     );
   }
 
+  // from json
+  static ShippingAddressModel fromJson(Map<String, dynamic> json) {
+    return ShippingAddressModel(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      address: json['address'] ?? '',
+      city: json['city'] ?? '',
+      addressDetails: json['addressDetails'] ?? '',
+      phoneNumber: json['phoneNumber'] ?? '',
+    );
+  }
+
+  ShippingAddressEntity toEntity() {
+    return ShippingAddressEntity(
+      name: name,
+      email: email,
+      address: address,
+      city: city,
+      addressDetails: addressDetails,
+      phoneNumber: phoneNumber,
+    );
+  }
+
   // Separate method to display shipping address
   String getAddress() {
     return '$address, $city, $addressDetails';
