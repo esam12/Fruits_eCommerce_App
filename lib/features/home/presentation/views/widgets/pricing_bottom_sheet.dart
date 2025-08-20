54cbcd919381705fc4e5b43a0dd3360f5fb5c9f7
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-08-20 22:11:39 +0300

    feat: Add pricing filter bottom sheet component - Implemented RangeSlider for price filtering

---
+++ lib/features/home/presentation/views/widgets/pricing_bottom_sheet.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:fruits/core/cubits/products_cubit/products_cubit.dart';
+import 'package:fruits/core/utils/constants/app_colors.dart';
+import 'package:fruits/core/utils/constants/app_text_styles.dart';
+import 'package:fruits/core/widgets/custom_button.dart';
+
+class PricingBarBottomSheet extends StatefulWidget {
+  const PricingBarBottomSheet({super.key, required this.cubit});
+  final ProductsCubit cubit;
+
+  @override
+  PricingBarBottomSheetState createState() => PricingBarBottomSheetState();
+}
+
+class PricingBarBottomSheetState extends State<PricingBarBottomSheet> {
+  RangeValues _currentRangeValues = const RangeValues(100, 500);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 5,
+              width: 60,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'تصنيف حسب :',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          const Row(
+            children: [
+              Icon(
+                Icons.discount_outlined,
+                size: 16.0,
+              ),
+              SizedBox(width: 8.0),
+              Text(
+                'السعر:',
+                style: TextStyles.bold13,
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 36,
+                width: 135,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: AppColors.greyTextColor),
+                ),
+                child: Center(
+                  child: Text(
+                    _currentRangeValues.start.round().toString(),
+                  ),
+                ),
+              ),
+              const Text(
+                'الي',
+                style: TextStyles.bold13,
+              ),
+              Container(
+                height: 36,
+                width: 135,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: AppColors.greyTextColor),
+                ),
+                child: Center(
+                  child: Text(
+                    _currentRangeValues.end.round().toString(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          RangeSlider(
+            values: _currentRangeValues,
+            min: 0,
+            max: 1000,
+            divisions: 25,
+            labels: RangeLabels(
+              _currentRangeValues.start.round().toString(),
+              _currentRangeValues.end.round().toString(),
+            ),
+            onChanged: (RangeValues values) {
+              setState(() {
+                _currentRangeValues = values;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          CustomButton(
+            text: 'تصفيه',
+            onPressed: () {
+              widget.cubit.getProducts(
+                minPrice: _currentRangeValues.start,
+                maxPrice: _currentRangeValues.end,
+              );
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
